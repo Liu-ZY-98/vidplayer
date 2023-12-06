ea69553b241cbe5817580206491ff4de3f648a74
Zhiyuan Liu <51340403+Liu-ZY-98@users.noreply.github.com>
2023-12-05 22:36:22 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById('video');
+    var playPauseButton = document.getElementById('play-pause');
+    var resetButton = document.getElementById('reset');
+
+    // Function to extract query parameters from the URL
+    function getQueryParam(param) {
+        var searchParams = new URLSearchParams(window.location.search);
+        return searchParams.get(param);
+    }
+
+    // Retrieve video source and start time from query parameters
+    var videoSource = getQueryParam('video'); // e.g., "video=example_video.mp4"
+    var startTime = getQueryParam('start'); // e.g., "start=120"
+
+    if (videoSource) {
+        video.src = videoSource;
+        video.load();
+    }
+
+    if (startTime) {
+        video.currentTime = parseFloat(startTime) / 30.0;
+    }
+
+    playPauseButton.addEventListener('click', function() {
+        if (video.paused || video.ended) {
+            playPauseButton.textContent = 'Pause';
+            video.play();
+        } else {
+            playPauseButton.textContent = 'Play';
+            video.pause();
+        }
+    });
+
+    resetButton.addEventListener('click', function() {
+        video.currentTime = 0; // Reset to the very start of the video
+        video.pause();
+        playPauseButton.textContent = 'Play';
+    });
+});
